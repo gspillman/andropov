b2013c7003861a4eae90cc90e09a8fda1bec4bea
Gary Spillman <gspillman-kgb@live.com>
2014-03-14 11:45:51 -0500

    Included validation for Report active record

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -41,8 +41,7 @@ class ReportsController < ApplicationController
   # POST /reports
   # POST /reports.json
   def create
-
-    @report = Report.new(params[:report]
+    @report = Report.new(params[:report])
 
     respond_to do |format|
       if @report.save
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ActiveRecord::Base
   attr_accessible :browser, :environment, :project, :result, :scenario
+  validates :result, presence: true
+  validates :environment, presence: true
+  validates :project, presence: true
+  validates :scenario, presence: true
 end
